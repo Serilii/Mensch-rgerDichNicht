3e6c435202e8794caa4f991595d900a2971d549c
Serilii <cem.erden@web.de>
2024-05-05 02:19:14 +0200

    die logik sitzt baby

---
+++ Mensch_ärgere_dich_nicht/src/klassen/Farbe.java
@@ -0,0 +1,16 @@
+package klassen;
+
+public class Farbe {
+    public Figur figur_1;
+    public Figur figur_2;
+    public Figur figur_3;
+    public Figur figur_4;
+    
+
+    public Farbe(String farbe) {
+       this.figur_1 = new Figur(farbe);
+       this.figur_2 = new Figur(farbe);
+       this.figur_3 = new Figur(farbe);
+       this.figur_4 = new Figur(farbe);
+    }
+}
