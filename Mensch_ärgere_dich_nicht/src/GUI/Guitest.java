e9a048670c239cb4b8344cc5644955d92d3ec87b
Serilii <cem.erden@web.de>
2024-05-23 14:15:04 +0200

    gui angepasst

--- Mensch_ärgere_dich_nicht/src/GUI/Guitest.java
+++ Mensch_ärgere_dich_nicht/src/GUI/Guitest.java
@@ -9,60 +9,64 @@ public class Guitest {
     JPanel panel_oben;
     JPanel panel_links;
     Spielbrett_Panel panel_mitte;
+    //JPanel würfelPanel;
 
     JButton button_links_1;
     JButton button_links_2;
     JButton button_links_3;
     JButton button_links_4;
     JButton button_oben;
+    //JButton Spieleranzeige;
 
     BoxLayout layout_links;
 
     JLayeredPane layerPane;
     JLayeredPane layerPane_buffer;
 
-    Spielfeld sf = new Spielfeld();
+    FigurPanel blau_1; 
+    FigurPanel blau_2;
+    FigurPanel blau_3; 
+    FigurPanel blau_4; 
+    FigurPanel[] array_FigurPanele_blau ;
 
-    FigurPanel blau_1 = new FigurPanel(sf.Blau.figur_1);
-    FigurPanel blau_2 = new FigurPanel(sf.Blau.figur_2);
-    FigurPanel blau_3 = new FigurPanel(sf.Blau.figur_3);
-    FigurPanel blau_4 = new FigurPanel(sf.Blau.figur_4);
+    FigurPanel gruen_1; 
+    FigurPanel gruen_2;
+    FigurPanel gruen_3;
+    FigurPanel gruen_4;
+    FigurPanel[] array_FigurPanele_gruen ;
 
-    FigurPanel gruen_1 = new FigurPanel(sf.Grün.figur_1);
-    FigurPanel gruen_2= new FigurPanel(sf.Grün.figur_2);
-    FigurPanel gruen_3= new FigurPanel(sf.Grün.figur_3);
-    FigurPanel gruen_4= new FigurPanel(sf.Grün.figur_4);
-    
-    FigurPanel rot_1 = new FigurPanel(sf.Rot.figur_1);
-    FigurPanel rot_2 = new FigurPanel(sf.Rot.figur_2);
-    FigurPanel rot_3 = new FigurPanel(sf.Rot.figur_3);
-    FigurPanel rot_4 = new FigurPanel(sf.Rot.figur_4);
+    FigurPanel rot_1; 
+    FigurPanel rot_2;
+    FigurPanel rot_3;
+    FigurPanel rot_4;
+    FigurPanel[] array_FigurPanele_rot;
 
-    FigurPanel gelb_1 = new FigurPanel(sf.Gelb.figur_1);
-    FigurPanel gelb_2 = new FigurPanel(sf.Gelb.figur_2);
-    FigurPanel gelb_3 = new FigurPanel(sf.Gelb.figur_3);
-    FigurPanel gelb_4 = new FigurPanel(sf.Gelb.figur_4);
+    FigurPanel gelb_1; 
+    FigurPanel gelb_2;
+    FigurPanel gelb_3;
+    FigurPanel gelb_4; 
+    FigurPanel[] array_FigurPanele_gelb ;
 
-    public Guitest(){
+    //es fehlt noch die funktionalität der buttons, ein JLabel oben um zu zeigen wer dran ist, und vielleicht eine würfel animation
+
+    //generiert das Fenster, zeichnet die Figuren rein vom Spielfeld die man übergibt
+    public Guitest(Spielfeld sf){
         fenster = new JFrame("Ich bin eine GUI");
         fenster.setSize(1100,1100);
         fenster.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         fenster.getContentPane().setBackground(Color.PINK);
         fenster.setResizable(false);
     
-        //main panel (borderlayout)
         panel_border_main = new JPanel();
         panel_border_main.setBounds(0,0,1100,1100);
         panel_border_main.setLayout(new BorderLayout());
         
         //Unterpanele 
         panel_oben  = new JPanel();
-        // panel_oben.setPreferredSize(new Dimension(880, 120));
         panel_links = new JPanel();
-        // panel_links.setPreferredSize(new Dimension(120, 880));
-        // JPanel panel_mitte = new JPanel();
         panel_mitte = new Spielbrett_Panel();
         panel_mitte.setDoubleBuffered(true);
+
         //buttons  
         button_links_1 = new JButton("Figur 1");
         button_links_2 = new JButton("Figur 2");
@@ -82,11 +86,47 @@ public class Guitest {
         layerPane_buffer.setBounds(0,0,1100, 1100);
         layerPane.setDoubleBuffered(true);
 
+        //erstelle die FigurenPanele, verbinde sie hin undzurück mit den übergebenen Figuren, und packe sie in arrays zum iterieren
+        blau_1 = new FigurPanel(sf.Blau.figur_1);
+        blau_2 = new FigurPanel(sf.Blau.figur_2);
+        blau_3 = new FigurPanel(sf.Blau.figur_3);
+        blau_4 = new FigurPanel(sf.Blau.figur_4);
+        array_FigurPanele_blau = new FigurPanel[] {blau_1, blau_2,  blau_3, blau_4};
+
+        gruen_1 = new FigurPanel(sf.Grün.figur_1);
+        gruen_2= new FigurPanel(sf.Grün.figur_2);
+        gruen_3= new FigurPanel(sf.Grün.figur_3);
+        gruen_4= new FigurPanel(sf.Grün.figur_4);
+        array_FigurPanele_gruen = new FigurPanel[] { gruen_1,  gruen_2,   gruen_3,  gruen_4};
+
+        rot_1 = new FigurPanel(sf.Rot.figur_1);
+        rot_2 = new FigurPanel(sf.Rot.figur_2);
+        rot_3 = new FigurPanel(sf.Rot.figur_3);
+        rot_4 = new FigurPanel(sf.Rot.figur_4);
+        array_FigurPanele_rot = new FigurPanel[] {rot_1, rot_2,  rot_3, rot_4};
+
+        gelb_1 = new FigurPanel(sf.Gelb.figur_1);
+        gelb_2 = new FigurPanel(sf.Gelb.figur_2);
+        gelb_3 = new FigurPanel(sf.Gelb.figur_3);
+        gelb_4 = new FigurPanel(sf.Gelb.figur_4);
+        array_FigurPanele_gelb = new FigurPanel[] {gelb_1, gelb_2,  gelb_3, gelb_4};
+
+        //ich schmeisse alle arrays[] in ein array[][] damit der code schöner aussieht bei 4 Iterationen hintereinander
+        FigurPanel[][] array_alle_FigurenPanel_arrays_lol = new FigurPanel[][] {array_FigurPanele_blau, array_FigurPanele_gelb, array_FigurPanele_gruen, array_FigurPanele_rot};
+
+        //iteriere die arrays[] in der liste der arrays[][]  ///was man nicht alles tut für Übersichtlichkeit
+        for (FigurPanel[] array: array_alle_FigurenPanel_arrays_lol) {
+        for (FigurPanel panel : array){
+            panel.setBounds(76 + 80 * panel.figur.feld.x_koordinate, 0 + 80 * panel.figur.feld.y_koordinate, panel.getHeight() ,panel.getHeight() );
+            layerPane.add(panel, Integer.valueOf(5));
+            }
+        }
+
         fenster.add(layerPane);
         fenster.setVisible(true);
-
     }
 
+    //nur zum bugfixen, kann später raus
     public void testbewegung(){
         blau_1.figur.alle_felder_ziehen(2);
         blau_1.setBounds(76 + 80 * blau_1.figur.feld.x_koordinate, 0 + 80 * blau_1.figur.feld.y_koordinate, blau_1.getHeight() ,blau_1.getHeight() );
@@ -97,16 +137,18 @@ public class Guitest {
         layerPane.revalidate();
     }
 
+    //verschiebt das Panel der übergebenen Figur
     public void bewege_figur_bild(Figur figur) {
         figur.figurpanel.setBounds(76 + 80 * figur.feld.x_koordinate, 0 + 80 * figur.feld.y_koordinate, figur.figurpanel.getHeight() ,figur.figurpanel.getHeight() );  
+        layerPane.repaint();
+        layerPane.revalidate();
     }
 
-    public void draw_field(){
+    //zeichnet 
+    public void zeichne_Gui(){
            
         panel_links.setLayout(layout_links);
-    
         panel_links.add(button_links_1);
-       
         panel_links.add(button_links_2);
         panel_links.add(button_links_3);
         panel_links.add(button_links_4);
@@ -115,39 +157,27 @@ public class Guitest {
         panel_border_main.add(panel_mitte, BorderLayout.CENTER);
         panel_border_main.add(panel_links, BorderLayout.WEST);
         panel_border_main.add(panel_oben, BorderLayout.NORTH);
-    
-                
-        
         layerPane.add(panel_border_main, Integer.valueOf(0));
 
-
-        blau_1.setBounds(76 + 80 * blau_1.figur.feld.x_koordinate, 0 + 80 * blau_1.figur.feld.y_koordinate, blau_1.getHeight() ,blau_1.getHeight() );
-        layerPane.add(blau_1, Integer.valueOf(5));
         blau_1.figur.einsteigen();
-
-        layerPane.add(gruen_1, Integer.valueOf(5));
         gruen_1.figur.einsteigen();
 
         layerPane.repaint();
         layerPane.revalidate();
-
-
     }
 
-    public void redraw(){
-
-
-            
+    //vielleicht später nützlich zum bugfixen
+    public void redraw(){            
             layerPane.repaint();
             layerPane.revalidate();
     }
 
     public static void main (String args []) throws InterruptedException {
 
-        Guitest Gui = new Guitest();
+        Spielfeld sf = new Spielfeld();
+        Guitest Gui = new Guitest(sf);
 
-        Gui.draw_field();
-
+        Gui.zeichne_Gui();
 
                 for (int i = 0; i < 50; i++ ){
                     Thread.sleep(100);
